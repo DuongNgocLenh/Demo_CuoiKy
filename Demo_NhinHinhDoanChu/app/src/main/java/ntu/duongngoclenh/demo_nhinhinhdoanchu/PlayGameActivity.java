2a77585a4aa6327ca9c620e29289596e6e0af628
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-04-29 10:03:02 +0700

    Tạo xml va xử lý sự kiện trong rules và activity

---
+++ Demo_NhinHinhDoanChu/app/src/main/java/ntu/duongngoclenh/demo_nhinhinhdoanchu/PlayGameActivity.java
@@ -0,0 +1,66 @@
+package ntu.duongngoclenh.demo_nhinhinhdoanchu;
+
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class PlayGameActivity extends AppCompatActivity {
+    ArrayList<String> ArrDapAn;
+    GridView gdvDapAn;
+    ArrayList<String> ArrNhapDapAn;
+    GridView gdvNhapDapAn;
+    private String dapAn="CADAO";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+        //Tim dieu khien
+        gdvDapAn = findViewById(R.id.gdvDapAn);
+        gdvNhapDapAn = findViewById(R.id.gdvNhapDapAn);
+        // Khởi tạo dữ liệu
+        ArrDapAn = new ArrayList<>();
+        ArrNhapDapAn = new ArrayList<>();
+        HienDungODapAn();
+        // Set số cột bằng đúng số lượng phần tử => nằm ngang
+        gdvDapAn.setNumColumns(ArrDapAn.size());
+        // Gán adapter
+        gdvDapAn.setAdapter(new DapAnAdapter(this, 0, ArrDapAn));
+        // Set số cột bằng đúng số lượng phần tử => nằm ngang
+        gdvNhapDapAn.setNumColumns(ArrNhapDapAn.size()/2);
+        // Gán adapter
+        gdvNhapDapAn.setAdapter(new DapAnAdapter(this, 0, ArrNhapDapAn));
+    }
+    private void HienDungODapAn() {
+        ArrDapAn.clear();
+        ArrNhapDapAn.clear();
+        Random random = new Random();
+
+        // Khởi tạo arrDapAn với các ô trống
+        for (int i = 0; i < dapAn.length(); i++) {
+            ArrDapAn.add("");
+        }
+
+        // Thêm các ký tự của dapAn (chữ hoa) vào arrNhapDapAn
+        for (int i = 0; i < dapAn.length(); i++) {
+            ArrNhapDapAn.add(String.valueOf(dapAn.charAt(i)).toUpperCase());
+        }
+
+        // Thêm các ký tự ngẫu nhiên từ A đến Z để đủ 10 ký tự
+        while (ArrNhapDapAn.size() < 10) {
+            char randomChar = (char) ('A' + random.nextInt(26));
+            ArrNhapDapAn.add(String.valueOf(randomChar));
+        }
+        // Xáo trộn các ký tự trong ArrNhapDapAn
+        Collections.shuffle(ArrNhapDapAn);
+    }
+}
---
+++ Demo_NhinHinhDoanChu/app/src/main/java/ntu/duongngoclenh/demo_nhinhinhdoanchu/RulesActivity.java
@@ -0,0 +1,28 @@
+package ntu.duongngoclenh.demo_nhinhinhdoanchu;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RulesActivity extends AppCompatActivity {
+    Button btnQuayLai;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+        btnQuayLai = findViewById(R.id.btnQuayLai);
+        btnQuayLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
