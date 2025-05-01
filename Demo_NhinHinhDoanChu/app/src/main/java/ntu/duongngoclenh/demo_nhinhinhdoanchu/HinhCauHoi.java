b46437dae0039d92de0604750b7deb193554b317
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-05-01 11:17:24 +0700

    update

---
+++ Demo_NhinHinhDoanChu/app/src/main/java/ntu/duongngoclenh/demo_nhinhinhdoanchu/HinhCauHoi.java
@@ -0,0 +1,13 @@
+package ntu.duongngoclenh.demo_nhinhinhdoanchu;
+public class HinhCauHoi {
+    public String tenNam, dapAn, hinhAnh;
+    public HinhCauHoi() {
+
+    }
+
+    public HinhCauHoi(String tenNam, String dapAn, String hinhAnh) {
+        this.tenNam = tenNam;
+        this.dapAn = dapAn;
+        this.hinhAnh = hinhAnh;
+    }
+}
---
+++ Demo_NhinHinhDoanChu/app/src/main/java/ntu/duongngoclenh/demo_nhinhinhdoanchu/PlayGameModel.java
@@ -0,0 +1,40 @@
+package ntu.duongngoclenh.demo_nhinhinhdoanchu;
+
+import java.util.ArrayList;
+
+public class PlayGameModel {
+    PlayGameActivity p;
+    ArrayList<HinhCauHoi> arr;
+    int cauSo = 0;
+    public NguoiChoi nguoiChoi;
+
+    public PlayGameModel(PlayGameActivity p) {
+        this.p = p;
+        nguoiChoi = new NguoiChoi();
+        khoiTaoDaTa();
+    }
+
+    private void khoiTaoDaTa() {
+        arr = new ArrayList<>();
+        arr.add(new HinhCauHoi("Màn 1", "yeuot", "https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-60.jpg"));
+        arr.add(new HinhCauHoi("Màn 2", "baocao", "https://freetuts.net/upload/product_series/images/2022/11/16/2598/nhin-hinh-doan-chu-2.jpg"));
+        arr.add(new HinhCauHoi("Màn 3", "cadao", "https://i.ytimg.com/vi/rHtDfzSPZfc/maxresdefault.jpg"));
+    }
+
+    public HinhCauHoi LayHinhCauHoi() {
+        if (cauSo >= arr.size()) {
+            cauSo = arr.size() - 1;
+        }
+        return arr.get(cauSo);
+    }
+
+    public void NextCauHoi() {
+        cauSo++;
+    }
+    public void layThongTin(){
+        nguoiChoi.getTT(p);
+    }
+    public void luuThongTin(){
+        nguoiChoi.saveTT(p);
+    }
+}
